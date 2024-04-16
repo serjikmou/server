b133b9d01d87848827c67019085a76dd96fa9c6c
Sajjad Mousavi <sm@qaap.oi>
2024-04-17 00:42:06 +0330

    edit

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+require("dotenv").config();
 const Joi = require("joi");
 const genres = require("./routes/genres");
 const express = require("express");
 const error = require("./middleware/error");
 const app = express();
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
-mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => console.log("connecting to mongoDB"))
-  .catch(() => console.log("Not connecting to mongoDB"));
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`connecting to mongoDB ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
@@ -19,5 +26,7 @@ app.use(express.json());
 app.use(error);
 app.use("/api/genres", genres);
 
+connectDB().then(() => {
+  app.listen(port, () => console.log(`Listening on port ${port}...`));
+});
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
